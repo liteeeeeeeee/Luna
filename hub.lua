6b14618829090ab0ab8e4bebe20a68b50e829b0f
lite <u94000067@gmail.com>
2025-01-25 23:24:03 +0300

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -30,3 +30,18 @@ local Window = Luna:CreateWindow({
 	}
 })
 
+local Tab = Window:CreateTab({
+	Name = "Tab Example",
+	Icon = "view_in_ar",
+	ImageSource = "Material",
+	ShowTitle = true -- This will determine whether the big header text in the tab will show
+})
+
+
+local Tab = Window:CreateTab({
+	Name = "Tab Example",
+	Icon = "view_in_ar",
+	ImageSource = "Material",
+	ShowTitle = true -- This will determine whether the big header text in the tab will show
+})
+
