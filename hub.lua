dc9793a720b1a76ddda6168c35a7f27e08a528b6
lite <u94000067@gmail.com>
2025-01-26 01:20:16 +0300

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,43 +1,48 @@
 local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/main/source.lua", true))()
+getgenv().lunainvite = "getluna"
 
 
 local Window = Luna:CreateWindow({
-	Name = "Luna Example Window", -- This Is Title Of Your Window
+	Name = "Home", -- This Is Title Of Your Window
 	Subtitle = nil, -- A Gray Subtitle next To the main title.
 	LogoID = "115516242896602", -- The Asset ID of your logo. Set to nil if you do not have a logo for Luna to use.
 	LoadingEnabled = true, -- Whether to enable the loading animation. Set to false if you do not want the loading screen or have your own custom one.
-	LoadingTitle = "Luna Interface Suite", -- Header for loading screen
+	LoadingTitle = "Luna ScriptHub", -- Header for loading screen
 	LoadingSubtitle = "by Nebula Softworks", -- Subtitle for loading screen
 
 	ConfigSettings = {
 		RootFolder = nil, -- The Root Folder Is Only If You Have A Hub With Multiple Game Scripts and u may remove it. DO NOT ADD A SLASH
 		ConfigFolder = "Big Hub" -- The Name Of The Folder Where Luna Will Store Configs For This Script. DO NOT ADD A SLASH
 	},
-
-	KeySystem = false, -- As Of Beta 6, Luna Has officially Implemented A Key System!
-	KeySettings = {
-		Title = "Luna Example Key",
-		Subtitle = "Key System",
-		Note = "Best Key System Ever! Also, Please Use A HWID Keysystem like Pelican, Luarmor etc. that provide key strings based on your HWID since putting a simple string is very easy to bypass",
-		SaveInRoot = false, -- Enabling will save the key in your RootFolder (YOU MUST HAVE ONE BEFORE ENABLING THIS OPTION)
-		SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-		Key = {"Example Key"}, -- List of keys that will be accepted by the system, please use a system like Pelican or Luarmor that provide key strings based on your HWID since putting a simple string is very easy to bypass
-		SecondAction = {
-			Enabled = true, -- Set to false if you do not want a second action,
-			Type = "Link", -- Link / Discord.
-			Parameter = "" -- If Type is Discord, then put your invite link (DO NOT PUT DISCORD.GG/). Else, put the full link of your key system here.
-		}
-	}
 })
 
 local Tab = Window:CreateTab({
-	Name = "Tab Example",
-	Icon = "view_in_ar",
+	Name = "Main",
+	Icon = "home",
 	ImageSource = "Material",
 	ShowTitle = true -- This will determine whether the big header text in the tab will show
 })
 
-Tab:CreateSection("Section Example")
+local Button = Tab:CreateButton({
+	Name = "Join our Discord!",
+	Description = nil, -- Creates A Description For Users to know what the button does (looks bad if you use it all the time),
+    	Callback = function()
+			local requestData = {
+				cmd = 'INVITE_BROWSER',
+				args = { code = getgenv().lunainvite },
+				nonce = game:GetService("HttpService"):GenerateGUID(false)
+			}
+			
+			local success, response = pcall((syn and syn.request) or http and http.request or http_request or (fluxus and fluxus.request) or request, {
+				Url = 'http://127.0.0.1:6463/rpc?v=1',
+				Method = 'POST',
+				Headers = { ['Content-Type'] = 'application/json', ['Origin'] = 'https://discord.com' },
+				Body = game:GetService("HttpService"):JSONEncode(requestData)
+			})     
+			
+    	end
+})
+
 
 
 local Tab = Window:CreateTab({
